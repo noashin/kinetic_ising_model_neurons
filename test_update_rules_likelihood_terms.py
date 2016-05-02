e6031dcf62bea8402d47bac6b5d33476fb082c82
noashin <noa.shinitski@gmail.com>
2016-05-02 14:49:57 +0200

    Update tests

--- test_update_rules_likelihood_terms.py
+++ test_update_rules_likelihood_terms.py
@@ -63,7 +63,7 @@ def test_update_s_i():
     m_i_new = np.array([3.0, 5.0, 4.0])
     mu_old = np.array([4.0, 1.0, 2.0])
 
-    result = update_s_i(z, v_i_new, v_old, m_i_new, mu_old)
+    result = update_s_i(z, v_i_new, v_old, m_i_new, mu_old, 1.0)
     expected_result = stats.norm.cdf(z) * np.sqrt(3) * 5 * np.exp(1.0 / 6.0 + 8.0 / 5.0 + 2.0 / 5.0) / (2.0*np.sqrt(2.0))
 
     np.testing.assert_almost_equal(result, expected_result)
@@ -91,7 +91,7 @@ def test_calc_alpha_i():
     v_old = np.array([2.0, 1.0, 0.0])
     z = 1.0
 
-    result = calc_alpha_i(x_i, v_old, z)
+    result = calc_alpha_i(x_i, v_old, z, 1.0)
     expected_result = (1.0 / np.sqrt(7.0)) * stats.norm.pdf(z) / stats.norm.cdf(z)
 
     np.testing.assert_array_equal(result, expected_result)
